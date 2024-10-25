1f8ff1031192270fdb999a563d6e432b1eaec334
hav-johwal <166103136+hav-johwal@users.noreply.github.com>
2024-10-25 15:53:58 +0200

    test: fixes deprecated usage of MockitoJUnitRunner

--- module/src/test/java/fish/focus/uvms/audit/DomainModelBeanTest.java
+++ module/src/test/java/fish/focus/uvms/audit/DomainModelBeanTest.java
@@ -13,7 +13,7 @@ package fish.focus.uvms.audit;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DomainModelBeanTest {
